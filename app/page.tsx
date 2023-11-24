8caff38dea11cca639f5cc787563fd29ac338a8f
eliaspasche <pasche.elias@googlemail.com>
2023-11-24 03:32:37 +0100

    fix: main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 "use client";
-import { usePathname, useRouter } from "next/navigation";
-import { useNavLinks } from "@/app/routes";
 
 export default function Home() {
-  const pathName = usePathname();
-  const router = useRouter();
-  const navLinks = useNavLinks();
-
-  if (pathName === "/") {
-    router.push(navLinks[0].path);
-  }
+  // const pathName = usePathname();
+  // const router = useRouter();
+  // const navLinks = useNavLinks();
+  //
+  // if (pathName === "/") {
+  //   router.push(navLinks[0].path);
+  // }
 
   return <div></div>;
 }
